cd04df1b97dcd8dac2ba665990affa94f359abe4
Diakon5 <72417806+Diakon5@users.noreply.github.com>
2023-02-22 14:14:26 +0100

    Made the patcher load the script items

--- UniversalRecipes/Contents/mods/UniversalRecipes/media/lua/shared/daikonAutopatch.lua
+++ UniversalRecipes/Contents/mods/UniversalRecipes/media/lua/shared/daikonAutopatch.lua
@@ -1,13 +1,48 @@
 local daikon = {
     RecipePatcher = {
-
     }
 }
+
 local function patchRecipes()
+    local scriptItems ={
+        canOpeners = PZArrayList:emptyList(),
+        hammers = PZArrayList:emptyList(),
+        screwdrivers = PZArrayList:emptyList(),
+        dullKnives = PZArrayList:emptyList(),
+        sharpKnives = PZArrayList:emptyList(),
+        scissors = PZArrayList:emptyList(),
+        weldingMasks = PZArrayList:emptyList(),
+        forks = PZArrayList:emptyList(),
+        disinfectants = PZArrayList:emptyList(),
+        liquor = PZArrayList:emptyList(),
+        milk = PZArrayList:emptyList(),
+        sugar = PZArrayList:emptyList(),
+        brokenGlass = PZArrayList:emptyList(),
+        sewingNeedle = PZArrayList:emptyList(),
+
+    }
+    Recipe.GetItemTypes.CanOpener(scriptItems.canOpeners)
+    Recipe.GetItemTypes.Hammer(scriptItems.hammers)
+    Recipe.GetItemTypes.Screwdriver(scriptItems.screwdrivers)
+    Recipe.GetItemTypes.DullKnife(scriptItems.dullKnives)
+    Recipe.GetItemTypes.SharpKnife(scriptItems.sharpKnives)
+    Recipe.GetItemTypes.Scissors(scriptItems.scissors)
+    Recipe.GetItemTypes.WeldingMask(scriptItems.weldingMasks)
+    Recipe.GetItemTypes.Fork(scriptItems.forks)
+    Recipe.GetItemTypes.Disinfectant(scriptItems.disinfectants)
+    Recipe.GetItemTypes.Liquor(scriptItems.liquor)
+    Recipe.GetItemTypes.Milk(scriptItems.milk)
+    Recipe.GetItemTypes.Sugar(scriptItems.sugar)
+    Recipe.GetItemTypes.BrokenGlass(scriptItems.brokenGlass)
+    Recipe.GetItemTypes.SewingNeedle(scriptItems.sewingNeedle)
     ---@type ScriptManager
     local scriptManager = ScriptManager.instance
     ---@type ArrayList
     local recipes = scriptManager:getAllRecipes()
+    print(scriptItems.liquor)
+    print(scriptItems.milk)
+    print(scriptItems.brokenGlass)
+    print(scriptItems.weldingMasks)
 	print("DAIKON UNIVERSAL RECIPES DEBUG")
     for i=0, recipes:size() - 1 do
         ---@type Recipe

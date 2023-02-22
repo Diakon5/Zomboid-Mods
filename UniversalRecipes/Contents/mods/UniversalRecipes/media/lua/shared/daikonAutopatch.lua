7baaeb872136509da64025858bfa43ce35f65e80
Diakon5 <72417806+Diakon5@users.noreply.github.com>
2023-02-22 13:30:16 +0100

    Made the patcher loadable and print recipes

--- UniversalRecipes/Contents/mods/UniversalRecipes/media/lua/shared/daikonAutopatch.lua
+++ UniversalRecipes/Contents/mods/UniversalRecipes/media/lua/shared/daikonAutopatch.lua
@@ -8,21 +8,26 @@ local function patchRecipes()
     local scriptManager = ScriptManager.instance
     ---@type ArrayList
     local recipes = scriptManager:getAllRecipes()
+	print("DAIKON UNIVERSAL RECIPES DEBUG")
     for i=0, recipes:size() - 1 do
         ---@type Recipe
         local recipe = recipes:get(i)
         --recipeSource is the list of items that can be used to make the recipe
-        ---@type Recipe.Source
+        ---@type ArrayList|Recipe.Source
         local recipeSource = recipe:getSource()
         for j=0, recipeSource:size()-1 do
-            local recipeItems = recipeSource:get(j):getItems()
-            ---@type string
-            local items = recipe:getName()
-            for k=0, recipeItems:size()-1 do
-                items = items.." "..recipeItems[k]
+            if recipeSource:get(j):isKeep() then
+                --replace with actual patcher code
+                local items = recipe:getName().."\n"
+                local recipeItems = recipeSource:get(j):getItems()
+                for k=0, recipeItems:size()-1 do
+                    items = items.." "..recipeItems:get(k)
+                end
+                print(items)
             end
-            print(items)
         end
     end
 end
+
+Events.OnGameBoot.Add(patchRecipes)
 return daikon

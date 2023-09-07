15c0fa01133099a8599ae0ee159a3a9cd7624ff1
Diakon5 <72417806+Diakon5@users.noreply.github.com>
2023-09-07 14:17:11 +0200

    Finished adding extra containers and patching items

--- ApocalypseFuel/Contents/mods/ApocalypseFuel/media/lua/shared/DaikonApoFuel_SetupFile.lua
+++ ApocalypseFuel/Contents/mods/ApocalypseFuel/media/lua/shared/DaikonApoFuel_SetupFile.lua
@@ -1,17 +1,20 @@
 Daikon =  Daikon or {}
 Daikon.ApocalypseBiofuels = {}
 Daikon.ApocalypseBiofuels.ItemsToMakeEthanolContainers = {
-    "Base.Saucepan" = "Biofuels.SaucepanWithEthanol",
-    "Base.Pot" = "Biofuels.PotWithEthanol",
-    "Base.BleachEmpty" = "Biofuels.BleachBottleWithEthanol",
-    "Base.PopBottleEmpty",
-    "Base.WaterBottleEmpty",
-    "Base.WhiskeyEmpty",
-    "Base.WineEmpty",
-    "Base.WineEmpty2"
+    ["Base.Saucepan"] = "Biofuels.SaucepanWithEthanol",
+    ["Base.Pot"] = "Biofuels.PotWithEthanol",
+    ["Base.BleachEmpty"] = "Biofuels.BleachBottleWithEthanol",
+    ["Base.PopBottleEmpty"] = "Biofuels.PopBottleWithEthanol",
+    ["Base.WaterBottleEmpty"] = "Biofuels.WaterBottleWithEthanol",
+    ["Base.WhiskeyEmpty"] = "Biofuels.WhiskeyBottleWithEthanol",
+    ["Base.BeerEmpty"] = "Biofuels.BeerBottleWithEthanol",
+    ["Base.WineEmpty"] = "Biofuels.WineBottleWithEthanol",
+    ["Base.WineEmpty2"] = "Biofuels.WineBottleWithEthanol"
 }
-
+local ScriptManager = ScriptManager.instance
 for emptyItem, fullItem in pairs(Daikon.ApocalypseBiofuels.ItemsToMakeEthanolContainers) do
-
+    local emptyItemActualItem = ScriptManager:getItem(emptyItem)
+    emptyItemActualItem:getTags():add("ApoFuelEmptyEthanol")
+    emptyItemActualItem:getReplaceTypesMap():putMapEntries(emptyItem,fullItem)
 end
 return Daikon

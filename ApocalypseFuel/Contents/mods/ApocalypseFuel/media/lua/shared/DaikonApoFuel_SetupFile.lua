d4a9707b87a87ae42caa570d06f9d360067ef0e9
Diakon5 <72417806+Diakon5@users.noreply.github.com>
2023-09-07 12:14:13 +0200

    Start making patcher and added new items

--- ApocalypseFuel/Contents/mods/ApocalypseFuel/media/lua/shared/DaikonApoFuel_SetupFile.lua
+++ ApocalypseFuel/Contents/mods/ApocalypseFuel/media/lua/shared/DaikonApoFuel_SetupFile.lua
@@ -3,7 +3,7 @@ Daikon.ApocalypseBiofuels = {}
 Daikon.ApocalypseBiofuels.ItemsToMakeEthanolContainers = {
     "Base.Saucepan" = "Biofuels.SaucepanWithEthanol",
     "Base.Pot" = "Biofuels.PotWithEthanol",
-    "Base.BleachEmpty",
+    "Base.BleachEmpty" = "Biofuels.BleachBottleWithEthanol",
     "Base.PopBottleEmpty",
     "Base.WaterBottleEmpty",
     "Base.WhiskeyEmpty",
@@ -11,4 +11,7 @@ Daikon.ApocalypseBiofuels.ItemsToMakeEthanolContainers = {
     "Base.WineEmpty2"
 }
 
+for emptyItem, fullItem in pairs(Daikon.ApocalypseBiofuels.ItemsToMakeEthanolContainers) do
+
+end
 return Daikon
